31613accebc485c4c9e08765a26ea8286e4c5343
Coolzero <lizux@users.noreply.github.com>
2026-07-09 14:23:49 +0800

    Update before-update-custom.sh

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -10,3 +10,7 @@
 
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+
+# 添加 Passwall 依赖源和主源
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall_apps https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
